df480b860b5ed9a3b2d58a2ec37a89e867f600fc
ReneeShkipper <marinkagnome@gmail.com>
2023-10-30 18:16:49 +0300

    Add project files.

---
+++ Homework_3_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Input number");
+int A = int.Parse(Console.ReadLine());
+int B;
+int Sum = 0;
+do
+{
+    B = A % 10;
+    A /= 10;
+    Sum += B;
+}
+while (A > 0);
+Console.WriteLine("Sum = " + Sum);
+Console.ReadLine();
